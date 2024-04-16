b7da04b6634e0f3d7f65dcc1e65d6c8d92199447
Heber <Sojuel1034@gmail.com>
2024-04-16 15:07:02 -0600

    Agregando validacion al formulario registro e insertar en base de datos si estos pasan

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,5 @@
+import { validationResult, check } from 'express-validator'
+
 import Usuario from '../models/Usuario.js'
 // render llama a la vista login
 // El redner toma primero la ruta, luego la informacion que va a pasar
@@ -16,6 +18,45 @@ const formularioRegistro = (req, res) => {
 
 //Para registrar a usuarios que vengan desde el formulario
 const registrar = async (req, res) => {
+  //Validacion
+  //Validar que el nombre no este vacio
+  await check('nombre')
+    .notEmpty()
+    .withMessage('El nombre es obligatorio')
+    .run(req)
+
+  //Validar que sea un email
+  await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+
+  //Validar que el email no este vacio
+  await check('email')
+    .notEmpty()
+    .withMessage('El email es obligatorio')
+    .run(req)
+
+  //Validar que el passwrod sea de mas de 6 caracteres
+  await check('password')
+    .isLength({ min: 2 })
+    .withMessage('El password debe tener mas de 8 caracteres')
+    .run(req)
+
+  //Validar que el password se repita y sea el correcto
+  await check('repetir_password')
+    .equals(req.body.password)
+    .withMessage('EL password no coincide')
+    .run(req)
+
+  //MOstramos el resultado de las validaciones anteriores
+  let resultado = validationResult(req)
+
+  //Verificar que la lista de errores este vacia
+  if (!resultado.isEmpty()) {
+    return res.render('auth/registro', {
+      pagina: 'Crear Cuenta',
+      errores: resultado.array()
+    })
+  }
+
   //Creamos usuario a partir de los datos que vienen del formulario
   const usuario = await Usuario.create(req.body)
   res.json(usuario)
