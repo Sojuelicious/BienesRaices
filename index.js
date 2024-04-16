9a9f974102d1c970eea31a26449f66be55b66bce
Heber <Sojuel1034@gmail.com>
2024-04-15 21:14:08 -0600

    Agregando conexion a base de datos MYSQL

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import usuarioRouter from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 
 // Creando la app
 const app = express()
+
+//!Conexion a la base de datos
+try {
+  await db.authenticate()
+  console.log('Conexion establecida correctamente')
+} catch (error) {
+  console.log(error)
+}
 
 // Habilitar PUG
 app.set('view engine', 'pug')
