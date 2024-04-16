f6a80e18457245d8ea11f0a6be21dfcf95c112ad
Heber <Sojuel1034@gmail.com>
2024-04-16 12:44:31 -0600

    Agregando el primer modelo, usuarios, con sequelize, para crear la primera tabla, Usuarios

---
+++ models/Usuario.js
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db.js'
+
+//Aqui creamos la tabla usuarios utilizando sequelize, el primer parametro es el nombre de la tabla y el segundo son los datos
+const Usuario = db.define('usuarios', {
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  token: {
+    type: DataTypes.STRING
+  },
+  confirmado: {
+    type: DataTypes.BOOLEAN
+  }
+})
