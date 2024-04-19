596a247108176673b2419c29fbd646d6fcbb4c9b
Heber <Sojuel1034@gmail.com>
2024-04-19 17:23:36 -0600

    Verificar si el token del usuario es valido y mostrando una vista de error si no lo es

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -106,17 +106,26 @@ const registrar = async (req, res) => {
 }
 
 //Funcion que comprueba una cuenta con el token
-const confirmar = (req, res) => {
+const confirmar = async (req, res) => {
   const { token } = req.params
-  console.log(token)
 
   //!Verificar si el token es valido
+  const usuario = await Usuario.findOne({ where: { token } })
+  console.log(usuario)
+
+  if (!usuario) {
+    return res.render('auth/confirmar-cuenta', {
+      pagina: 'Error al confirmar tu cuenta',
+      mensaje: 'Error al confirmar la cuenta, intenta de nuevo',
+      error: true
+    })
+  }
 
   //!Confirmar la cuenta
 }
 
 const formularioOlvidePassword = (req, res) => {
-  res.render('auth/olvide-password', {
+  return res.render('auth/olvide-password', {
     pagina: 'Recuperar Cuenta'
   })
 }
