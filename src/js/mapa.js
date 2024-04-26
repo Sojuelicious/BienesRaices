a4cbeff50c95061b980725d7d1911902a96dbe60
Heber <Sojuel1034@gmail.com>
2024-04-26 11:29:59 -0600

    Propiedades - MAPA - Agregando pin al mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,11 +2,16 @@
   const lat = 14.6397057
   const lng = -91.2295308
   const mapa = L.map('mapa').setView([lat, lng], 16)
+  let marker
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mapa)
-})()
 
-alert('Sigo siendo el mapa444')
+  //PIN
+  marker = new L.marker([lat, lng], {
+    draggable: true,
+    autoPan: true
+  }).addTo(mapa)
+})()
