5c7e86dccf1444b994c388d4c94c92650b3a97fc
Heber <Sojuel1034@gmail.com>
2024-04-26 14:31:34 -0600

    Propiedades - SEEDER - Insertar precios y categorias a la db

---
+++ seed/seeder.js
@@ -0,0 +1,34 @@
+import precios from './Precio.js'
+import categorias from './Categorias.js' //Datos
+import Categoria from '../models/Categoria.js' //Modelo de la tabla de la bd
+import Precio from '../models/Precio.js'
+import db from '../config/db.js' //Instancia de la base de datos
+
+const importarDatos = async () => {
+  try {
+    //Autenticar en la db
+
+    await db.authenticate()
+
+    //Generar las columnas
+
+    await db.sync()
+
+    //insertar los datos
+
+    //? Le pasamos los datos que vienen de categorias como parametro
+    await Promise.all([
+      Categoria.bulkCreate(categorias),
+      Precio.bulkCreate(precios)
+    ])
+    console.log('Datos insertados correctamente')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1) //! IMPORTANTE Terminar el proceso si encuentra un error
+  }
+}
+
+if (process.argv[2] === '-i') {
+  importarDatos()
+}
