f7135a1230148dc674616b78e2056ef347b930c8
Heber <Sojuel1034@gmail.com>
2024-04-26 13:47:47 -0600

    Propiedades - Creando el modelo de db de Propiedades

---
+++ models/Propiedad.js
@@ -0,0 +1,51 @@
+import { DataTypes, INTEGER } from 'sequelize'
+import db from '../config/db.js'
+import { data } from 'autoprefixer'
+
+const Propiedad = db.define('propiedades', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  },
+  titulo: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  descripcion: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  habitaciones: {
+    DataTypes: INTEGER,
+    allowNull: false
+  },
+  estacionamiento: {
+    DataTypes: INTEGER,
+    allowNull: false
+  },
+  wc: {
+    DataTypes: INTEGER,
+    allowNull: false
+  },
+  calle: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  latitud: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  imagen: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  publicado: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
+})
+
+export default Propiedad
