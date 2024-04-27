394e2b2ee58eefa11c0fcb8d3abdfce1d4e69aa6
Heber <Sojuel1034@gmail.com>
2024-04-26 19:56:38 -0600

    Propiedades - Añadiendo validaciones finales al formulario

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -13,6 +13,23 @@ router.post(
   '/propiedades/crear',
   //Aplicando Validaciones
   body('titulo').notEmpty().withMessage('El titulo es obligatorio'),
+  body('descripcion')
+    .notEmpty()
+    .withMessage('Debe añadir una descripcion')
+    .isLength({ max: 200 })
+    .withMessage('La descripcion debe 200 caracteres como maximo'),
+  body('categoria').notEmpty().withMessage('Selecciona una categoría'),
+  body('precio').notEmpty().withMessage('Selecciona un rango de Precios'),
+  body('habitaciones')
+    .isNumeric()
+    .withMessage('Seleccione una cantidad de habitaciones'),
+  body('estacionamiento')
+    .isNumeric()
+    .withMessage('Seleccione cantidad de espacio de estacionamientos'),
+  body('wc').isNumeric().withMessage('Seleccione la cantidad de baños'),
+  body('latitud')
+    .notEmpty()
+    .withMessage('Seleccione una hubicacion en el mapa'),
 
   guardar
 )
